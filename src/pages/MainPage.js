1e476e6c463f07a770f9f83a3fc0abb097eee685
claracoud <clara.coudoin@gmail.com>
2020-12-20 10:03:48 +0100

    commit clara

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,19 +1,27 @@
 import React from "react";
 import "./mainPage.css";
 import hamburger from "../images/hamburger.jpg";
+import poireau from "../images/poireau-small.jpg";
+import poivron from "../images/poivron.jpg";
+
 
 function MainPage() {
   return (
     <div className="main-page-body">
       <div className="main-page-center">
+      <img className="poireau" src={poireau} />
+      <div className="recette">
         <h1>Trouver ma recette version végé</h1>
         <body>Toute recette est déclinable en version végétarienne, retrouvez vos plats préférés en version végé !</body>
         <input id="recepie-sb"></input>
+      </div>
+      <div className="aleatoire">
         <h1>Surprends moi !!</h1>
         <body>A court d'inspiration ? On se charge de trouver une recette pour vous</body>
         <button className="mode-button">Générer une recette aléatoire</button>
       </div>
-      <h1>Nos dernières recettes</h1>
+      </div>
+      <h1 className="blog">Nos dernières recettes</h1>
       <div className="main-page-foot">
         <img src={hamburger} />
         <img src={hamburger} />
